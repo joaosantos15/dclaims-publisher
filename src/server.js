a55a5247c2c0e8a307b73ba6a4b1fafdf0859439
Joao Santos <jrmsantos15@gmail.com>
2018-01-04 03:17:15 +0000

    bug fixes

--- src/server.js
+++ src/server.js
@@ -1,5 +1,4 @@
 const express = require('express')
-const bodyParser = require('body-parser')
 const ClaimsBuffer = require('./app/claimsBuffer.js')
 const Issue = require('./issue.js')
 const HypercertsCore = require('hypercerts-core')
